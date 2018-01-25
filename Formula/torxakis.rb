b5189635522a51f76a2a59c341e25491f8e0b7a8
Harco Kuppens <h.kuppens@cs.ru.nl>
2018-01-25 22:20:55 +0100

    Formula which build bottle is 'Formula/torxakis.rb' with bottle section removed. This because the formula will be included in the bottle.

--- Formula/torxakis.rb
+++ Formula/torxakis.rb
@@ -6,19 +6,26 @@ class Torxakis < Formula
   sha256 "03b370d2145206769e6cd12935404c5235a2afa6a779aef0aa3d0030057c6375"
   head "https://github.com/TorXakis/TorXakis.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://raw.githubusercontent.com/harcokuppens/homebrew-homebrew/master/bottles/"
+    sha256 "7c9ebc6abb1edb4bef5977cf85164cbf0aa3344ddcbf102e0aa1cc344645e337" => :high_sierra
+  end
+
   depends_on "haskell-stack" => :build
   depends_on "z3" => :recommended
   depends_on "cvc4" => :optional
 
   def install
-    system "stack", "--stack-yaml=stack_linux.yaml", "setup"
-    system "stack", "--stack-yaml=stack_linux.yaml", "build"
+    ohai "running install"
+    jobs = ENV.make_jobs
+    system "stack", "-j#{jobs}", "--stack-yaml=stack_linux.yaml", "setup"
+    system "stack", "-j#{jobs}", "--stack-yaml=stack_linux.yaml", "--local-bin-path=#{bin}", "install"
   end
 
   test do
-    ohai "running unit tests"
-    system "stack", "--stack-yaml=stack_linux.yaml", "test"
-    ohai "running integration tests"
-    system "stack", "--stack-yaml=test/sqatt/stack.yaml", "test"
+    ohai "running basic test"
+    output_torxakis = pipe_output('printf "eval 33+7777777777777\nq" |torxakis  2>&1')
+    assert_match(/7777777777810/, output_torxakis, 'torxakis failed in doing "eval 33+7777777777777"')
   end
 end
